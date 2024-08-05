1b106073f4b68cf9e554f25b9b9902bee572f9f7
Akhil Deshpande <deshpandeakhils@gmail.com>
2024-08-04 20:18:09 -0500

    quick mobile fix

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,19 @@
-import "./AboutMe.css";
-import "bulma/css/bulma.min.css";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './AboutMe.css';
+import 'bulma/css/bulma.min.css';
 
 function AboutMe() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        function handleResize() {
+            setIsMobile(window.innerWidth < 768);
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <div className="about-me-wrapper">
             <div className="about-me box has-background-dark">
@@ -20,14 +31,12 @@ function AboutMe() {
                 </div>
                 <div className="abt-me-ps">
                     <p className="has-text-light about-me-info">Hello there! My name is Akhil Deshpande. Earlier this year, I graduated with a B.S. in Physics from the University of Texas at Austin. I am currently a new graduate employed by <a href="https://usa.visa.com" target="_blank">Visa</a> as a Software Test Engineer in Austin, TX. My main interests these days revolve around creative programming solutions to everyday problems I face in the real world.</p>
-                    <p className="about-me-info"><br />To the right, you can see two of of my personal tracker projects - <a href="https://github.com/adeshpande03/github-readme-streak-stats/" target="_blank">Github Streak Stats</a> (bottom, forked from a public repository), as well as my original <a target="_blank" href="github.com/adeshpande03/leetCode-Points-Graph">Leetcode Experience Tracker</a> (top), both of which update in real time!</p>
+                    <p className="about-me-info">To the right, you can see two of my personal tracker projects - <a href="https://github.com/adeshpande03/github-readme-streak-stats/" target="_blank">Github Streak Stats</a> (bottom, forked from a public repository), as well as my original <a target="_blank" href="github.com/adeshpande03/leetCode-Points-Graph">Leetcode Experience Tracker</a> (top), both of which update in real time!</p>
                 </div>
             </div>
             <div className="imgs">
-                <img src="https://github.com/adeshpande03/LeetCode-Points-Graph/blob/main/points_graph_rounded_corners.png?raw=true" alt="" />
-                {/* <div className="streak-img-container"> */}
+                <img src="https://github.com/adeshpande03/LeetCode-Points-Graph/blob/main/points_graph_rounded_corners.png?raw=true" alt="LeetCode Points Graph" />
                 <img className="streak-img-container" src="https://github-readme-streak-stats-kappa-three.vercel.app?user=adeshpande03&theme=adeshpande03&border_radius=10&hide_longest_streak=true&hide_border=true" alt="GitHub Streak" />
-                {/* </div> */}
             </div>
         </div>
     );
