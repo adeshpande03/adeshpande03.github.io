da429be8472e39719c817cfbde487d5c2568b379
Akhil Deshpande <deshpandeakhils@gmail.com>
2024-08-07 15:38:36 -0500

    project mobile/web designed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,18 @@ import projects from "../Projects.json"
 
 function Projects() {
     const [isMobile, setIsMobile] = useState(window.innerWidth < 1250);
+    function sortProjects(projects) {
+        let sortedData
+        sortedData = projects.sort(function (a, b) {
+            let x = a.name.toLowerCase();
+            let y = b.name.toLowerCase();
+            if (x > y) { return 1; }
+            if (x < y) { return -1; }
+            return 0;
+        });
 
+        return sortedData
+    }
     useEffect(() => {
         function handleResize() {
             setIsMobile(window.innerWidth < 1250);
@@ -23,15 +34,16 @@ function Projects() {
                 </div>
             </section>
 
-            {projects.map((project) => (
+            {sortProjects(projects).map((project) => (
                 <div key={project.id} className="card has-background-dark" data-aos="fade-up">
                     <div className="card-content">
                         <p className="title">
-                            {project.name}
+                            <a target="_blank" href={"https://" + project.url}>{project.name}</a>
                         </p>
-                        <p className='subtitle'>{project.languages.join(', ')}</p>
+                        <p className='subtitle'>{project.languages.sort().join(', ')}</p>
                         <div className="card-content ">
-                            <div className="content">
+                            <div style={{"font-size":"larger", "color":"#fff"}}className="content">
+                                {project.content}
                             </div>
                         </div>
                     </div>
